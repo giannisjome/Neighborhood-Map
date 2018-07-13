def265041ba59abbf2c164a25a051bd33efdb958
giannisjome <giannisjome@hotmail.com>
2018-07-13 18:39:45 +0300

    Add files via upload

---
+++ js/places.js
@@ -0,0 +1,57 @@
+
+// My list of baseline restaurant objects.  "ID" is the key for the Foursquare
+// API call.  Name is manually entered here for convenience.
+// Neighborhood data is manually entered; had a hard time finding a reasonable
+// API to automatically generate neighboor data by location.
+
+var restObjArray = [
+    {
+        'name': 'Hacienda Cafe',
+        'id': '524c4d7d11d208221dad1377',
+        'notes': 'One of the best places for enjoying coffee.',
+        'neighborhood': 'Mastampas'
+    },
+
+    {
+        'name': 'La minita',
+        'id': '5b1a27a93d4791002ccd44d3',
+        'notes': 'The best take-away cafe in the city',
+        'neighborhood': 'Atsalenio'
+    },
+
+    {
+        'name': 'Miniatoura',
+        'id': '522b44cc498ede28e28f6770',
+        'notes': 'Enjoy freddo espresso, a cold version of espresso.',
+        'neighborhood': 'City center'
+    },
+
+    {
+        'name': 'Crop',
+        'id': '528e6c2411d25a4a9fdf53b3',
+        'notes': 'Speciality Coffees from all over the world',
+        'neighborhood': 'City center'
+    },
+
+    {
+        'name': 'STEAM',
+        'id': '571e3ab9498ed52e1151b031',
+        'notes': 'A small and quiet place for those who want something different.',
+        'neighborhood': 'Katsampas'
+    },
+
+    {
+        'name': 'Hacienda Cafe',
+        'id': '58b1dd2a58c4d129f8c9a5d4',
+        'notes': 'This is another spot of this cafe. Amazing coffees and great services!',
+        'neighborhood': 'Katsampas'
+    },
+
+    {
+        'name': 'Think tank',
+        'id': '500d99dce4b05106222507bd',
+        'notes': 'Amazing view of the city! Great place for a coffee addicted person!',
+        'neighborhood': 'City center'
+    },
+
+];
